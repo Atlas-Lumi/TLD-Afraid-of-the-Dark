545c69ce6f6577b527673ad0f3dac70b9dfe372d
kitte <kitte@DESKTOP-T9EFEHQ>
2022-04-11 20:53:14 +0100

    Add project files.

---
+++ AnxietyAtNight/Patches.cs
@@ -0,0 +1,147 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using System;
+
+namespace AnxietyAtNight
+{
+    internal class Patches
+    {
+        [HarmonyPatch(typeof(Anxiety), "Update")]
+        internal class Anxiety_Update
+        {
+            private static bool affliction;
+
+            public static bool HoldingLitTorch()
+            {
+                return (GameManager.GetPlayerManagerComponent().m_ItemInHands && GameManager.GetPlayerManagerComponent().m_ItemInHands.m_TorchItem && GameManager.GetPlayerManagerComponent().m_ItemInHands.m_TorchItem.IsBurning());
+            }
+            private static bool IsNight()
+            {
+                return GameManager.GetTimeOfDayComponent().IsNight();
+            }
+            private static bool IsDay()
+            {
+                return GameManager.GetTimeOfDayComponent().IsDay();
+            }
+            private static bool IsInShelter()
+            {
+                return GameManager.GetSnowShelterManager().PlayerInShelter();
+            }
+            private static bool IsInBuilding()
+            {
+                if (GameManager.GetWeatherComponent().IsIndoorScene())
+                {
+                    return true;
+                }
+                else if (GameManager.GetWeatherComponent().IsIndoorEnvironment())
+                {
+                    return true;
+                }
+                else 
+                { 
+                    return false; 
+                }
+            }
+            private static bool IsInCar()
+            {
+                return GameManager.GetPlayerInVehicle().IsInside();
+            }
+            private static void DoCameraPulse(float amount)
+            {
+                GameManager.GetCameraEffects().HeadachePulse(amount);
+            }
+            private static float GetDistanceToClosestFire()
+            {
+                return GameManager.GetFireManagerComponent().GetDistanceToClosestFire(GameManager.GetPlayerTransform().position);
+            }
+            private static void Play_AfflictionAnxiety()
+            {
+                GameManager.GetPlayerVoiceComponent().Play("Play_AnxietyAffliction", Voice.Priority.Critical);
+            }
+            private static void Play_AfflictionAnxietyCured()
+            {
+                GameManager.GetPlayerVoiceComponent().Play("Play_VOCatchBreath", Voice.Priority.Critical);
+            }
+
+            private static void Prefix(Anxiety __instance)
+            {
+                void AnxietyCuredPopup()
+                {
+                    PlayerDamageEvent.SpawnAfflictionEvent(__instance.m_AnxietyLocalizedString.m_LocalizationID, "GAMEPLAY_Healed", __instance.m_AnxietyIcon, InterfaceManager.m_FirstAidBuffColor);
+                }
+
+                if (IsNight())
+                {
+                    bool scaredConditionFire = GetDistanceToClosestFire() > 3f;
+                    bool scaredConditionTorch = HoldingLitTorch();
+                    bool scaredConditionInCar = IsInCar();
+                    bool scaredConditionShelter = IsInShelter();
+                    bool scaredConditionBuilding = IsInBuilding();
+
+                    // None of the conditions are met, anxiety added
+                    if (!affliction && scaredConditionFire && !scaredConditionTorch && !scaredConditionInCar && !scaredConditionShelter && !scaredConditionBuilding)
+                    {
+                        __instance.StartAffliction();
+                        Play_AfflictionAnxiety();
+                        DoCameraPulse(1f);
+                        affliction = true;
+                    }
+                    // Close to fire, anxiety removed
+                    else if (affliction && !scaredConditionFire && !scaredConditionTorch && !scaredConditionInCar)
+                    {
+                        __instance.StopAffliction(true);
+                        Play_AfflictionAnxietyCured();
+                        AnxietyCuredPopup();
+                        DoCameraPulse(1f);
+                        affliction = false;
+                    }
+                    // Holding lit torch, anxiety removed
+                    else if (affliction && scaredConditionFire && scaredConditionTorch && !scaredConditionInCar)
+                    {
+                        __instance.StopAffliction(true);
+                        Play_AfflictionAnxietyCured();
+                        AnxietyCuredPopup();
+                        DoCameraPulse(1f);
+                        affliction = false;
+                    }
+                    // Inside a car, anxiety removed
+                    else if (affliction && scaredConditionInCar)
+                    {
+                        __instance.StopAffliction(true);
+                        Play_AfflictionAnxietyCured();
+                        AnxietyCuredPopup();
+                        DoCameraPulse(1f);
+                        affliction = false;
+                    }
+                    // Inside snow shelter, anxiety removed
+                    else if (affliction && scaredConditionShelter)
+                    {
+                        __instance.StopAffliction(true);
+                        Play_AfflictionAnxietyCured();
+                        AnxietyCuredPopup();
+                        DoCameraPulse(1f);
+                        affliction = false;
+                    }
+                    // Inside a building, shelter, or cave, anxiety removed
+                    else if (affliction && scaredConditionBuilding)
+                    {
+                        __instance.StopAffliction(true);
+                        Play_AfflictionAnxietyCured();
+                        AnxietyCuredPopup();
+                        DoCameraPulse(1f);
+                        affliction = false;
+                    }
+                }           
+                // Day time, anxiety removed
+                else if (IsDay()&& affliction)
+                {
+                    __instance.StopAffliction(true);
+                    DoCameraPulse(1f);
+                    Play_AfflictionAnxietyCured();
+                    AnxietyCuredPopup();
+                    affliction = false;
+                }              
+            }
+        }
+    }
+}
---
+++ AnxietyAtNight/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+[assembly: AssemblyTitle("AfraidOfTheDark")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AfraidOfTheDark")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cc48b3c2-e286-4f97-89f4-a9297eb67ec0")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: MelonInfo(typeof(AnxietyAtNight.Implementation), "Afraid of the Dark", "1.0", "Atlas Lumi")]
+[assembly: MelonGame("Hinterland", "TheLongDark")]
